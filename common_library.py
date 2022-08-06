8d6eff75a760672b74aff5cb7cce891597bbf216
cwadven <cwadven@naver.com>
2022-08-06 18:29:58 +0900

    feat: 이메일 전송 함수
    - 이메일 전송 함수

--- common_library.py
+++ common_library.py
@@ -4,6 +4,9 @@ from botocore.config import Config
 from botocore.exceptions import ClientError
 
 from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 from rest_framework.exceptions import APIException
 
@@ -128,3 +131,24 @@ def generate_presigned_url(file_name, expires_in=1000):
         return url
     except ClientError as e:
         raise APIException(e)
+
+
+def send_email(title: str, html_body_content: str, payload: dict, to: list) -> None:
+    """
+    title: 메일 제목
+    html_body_content: 적용할 templates 폴더에 있는 html 파일 위치
+    payload: 해당 template_tag 로 쓰일 값들
+    to: 보낼 사람들 (리스트로 전달 필요)
+    """
+    message = render_to_string(
+        html_body_content,
+        payload
+    )
+    send_mail(
+        title,
+        strip_tags(message),
+        settings.EMAIL_HOST_USER,
+        to,
+        html_message=message,
+        fail_silently=False,
+    )
