19a7e3ac991d309f4683fb260eefdfb9749c6261
cwadven <cwadven@naver.com>
2022-08-06 18:26:48 +0900

    feat: s3 presigned url
    - presigned url 생성

--- common_library.py
+++ common_library.py
@@ -1,3 +1,6 @@
+import boto3 as boto3
+from django.conf import settings
+
 from rest_framework.exceptions import APIException
 
 from config.common.reponse_codes import PageSizeMaximumException
@@ -99,3 +102,25 @@ def get_request_ip(request):
     if x_forwarded_for:
         return x_forwarded_for.split(',')[0]
     return request.META.get('REMOTE_ADDR')
+
+
+def generate_presigned_url(file_name, expires_in=1000):
+    s3_client = boto3.client(
+        's3',
+        region_name='ap-northeast-2',
+        aws_access_key_id=settings.AWS_IAM_ACCESS_KEY,
+        aws_secret_access_key=settings.AWS_IAM_SECRET_ACCESS_KEY,
+        config=Config(signature_version='s3v4')
+    )
+    try:
+        url = s3_client.generate_presigned_url(
+            ClientMethod='put_object',
+            Params={
+                'Bucket': settings.AWS_S3_BUCKET_NAME,
+                'Key': f'{uuid.uuid4()}_{file_name}'
+            },
+            ExpiresIn=expires_in
+        )
+        return url
+    except ClientError as e:
+        raise APIException(e)
