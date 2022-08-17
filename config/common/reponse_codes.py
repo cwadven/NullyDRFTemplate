38981b0bdc43a5ebc34498968f20566753c2b8d9
cwadven <cwadven@naver.com>
2022-08-17 22:55:34 +0900

    feat: 로그인 실패 fail code 추가
    - 로그인 실패 fail code 추가

--- config/common/reponse_codes.py
+++ config/common/reponse_codes.py
@@ -5,3 +5,9 @@ class PageSizeMaximumException(APIException):
     status_code = 400
     default_detail = '사이즈를 초과했습니다.'
     default_code = 'page-size-maximum'
+
+
+class LoginFailedException(APIException):
+    status_code = 400
+    default_detail = '로그인에 실패했습니다.'
+    default_code = 'login-error'
