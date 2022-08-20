4058ecbda5aaad6f2e836f9c87ce263d2c76fad7
cwadven <cwadven@naver.com>
2022-08-20 16:29:41 +0900

    feat: api 예외처리 수정
    - api 예외처리 방법 수정

--- config/middleware/api_exception.py
+++ config/middleware/api_exception.py
@@ -1,66 +1,46 @@
-from datetime import datetime
-
 from rest_framework.views import exception_handler
 from rest_framework import exceptions
-from rest_framework.response import Response
 
-from config.settings import logger
+from config.settings.base import logger
 
 
 def custom_exception_handler(exc, context):
-    logger.error(f"[CUSTOM_EXCEPTION_HANDLER_ERROR]")
-    logger.error(f"[{datetime.now()}]")
-    logger.error(f"> exc")
-    logger.error(f"{exc}")
-    logger.error(f"> context")
-    logger.error(f"{context}")
+    print('ER|%s| %s' % (exc, context))
+    logger.error(f'{exc}, {context}')
 
     response = exception_handler(exc, context)
 
     if response is not None:
         if isinstance(exc, exceptions.ParseError):
-            code = response.status_code
             msg = exc.detail
         elif isinstance(exc, exceptions.AuthenticationFailed):
-            code = response.status_code
             msg = exc.detail
         elif isinstance(exc, exceptions.NotAuthenticated):
-            code = response.status_code
-            msg = exc.detail
+            msg = 'No Auth'
         elif isinstance(exc, exceptions.PermissionDenied):
-            code = response.status_code
             msg = exc.detail
         elif isinstance(exc, exceptions.NotFound):
-            code = response.status_code
             msg = exc.detail
         elif isinstance(exc, exceptions.MethodNotAllowed):
-            code = response.status_code
             msg = exc.detail
         elif isinstance(exc, exceptions.NotAcceptable):
-            code = response.status_code
             msg = exc.detail
         elif isinstance(exc, exceptions.UnsupportedMediaType):
-            code = response.status_code
             msg = exc.detail
         elif isinstance(exc, exceptions.Throttled):
-            code = response.status_code
             msg = exc.detail
         elif isinstance(exc, exceptions.ValidationError):
-            code = response.status_code
             msg = exc.detail
-        else:
-            code = response.status_code
-            msg = "unknown error"
+        elif isinstance(exc, exceptions.APIException):
+            msg = exc.detail
 
-        response.status_code = 200
-        response.data['code'] = code
-        response.data['message'] = msg
-        response.data['data'] = None
+        response.data['error'] = msg
 
-        response.data.pop('detail', None)
+        try:
+            del response.data['detail']
+        except:
+            pass
+
         return response
     else:
-        return Response({
-            "code": "internal-error",
-            "default_message": "unknown error occurred.",
-        }, status=200)
+        return response
