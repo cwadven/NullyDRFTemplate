c521e14128e5110e4f0e994edb5bd1b47fd452de
cwadven <cwadven@naver.com>
2022-08-20 16:35:02 +0900

    chore: trailing 슬래쉬 제거
    - trailing 슬래쉬 제거

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from account.views import SocialLoginView
 
 urlpatterns = [
-    path('/social-login', SocialLoginView.as_view(), name='social_login'),
+    path('social-login', SocialLoginView.as_view(), name='social_login'),
 ]
